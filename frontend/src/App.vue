e7a4a127ed38121b90819706e3aeb50559775e48
akihiro-kondo <2107sum@gmail.com>
2024-11-30 03:16:22 +0900

    Add: 初回コミット

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: ''
+    };
+  },
+  mounted() {
+    axios.get('http://localhost:8888/api/data')
+      .then(response => {
+        this.message = response.data.message;
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }
+};
+</script>
