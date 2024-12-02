3478e7a3525326fe7ed9e65dc911682188d2fdfb
akihiro-kondo <2107sum@gmail.com>
2024-12-02 15:04:27 +0900

    Fix: vueの拡張フォーマット機能の確認

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,34 +1,33 @@
 <template>
     <div>
-      <h2>カテゴリー: {{ category.name }}</h2>
-      <p>{{ categoryContent }}</p>
+        <h2>カテゴリー: {{ category.name }}</h2>
+        <p>{{ categoryContent }}</p>
     </div>
-  </template>
-  
-  <script>
-  export default {
+</template>
+
+<script>
+export default {
     props: {
-      category: Object,
+        category: Object,
     },
     name: "CategoryPage",
     data() {
-      return {
-        categoryContent: null,
-      };
+        return {
+            categoryContent: null,
+        };
     },
     mounted() {
-      this.fetchCategoryContent();
+        this.fetchCategoryContent();
     },
     methods: {
-      fetchCategoryContent() {
-        // 選択されたカテゴリーの学習内容をAPIから取得
-        fetch(`http://localhost:8888/category/${this.category.id}`)
-          .then((response) => response.json())
-          .then((data) => {
-            this.categoryContent = data.content;
-          });
-      },
+        fetchCategoryContent() {
+            // 選択されたカテゴリーの学習内容をAPIから取得
+            fetch(`http://localhost:8888/category/${this.category.id}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    this.categoryContent = data.content;
+                });
+        },
     },
-  };
-  </script>
-  
+};
+</script>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,47 +1,46 @@
 <template>
     <div>
-      <h1>ダッシュボード</h1>
-      <ul>
-        <li v-for="category in categories" :key="category.id">
-          <button @click="fetchCategoryContent(category.id)">
-            {{ category.name }}
-          </button>
-        </li>
-      </ul>
-      <div v-if="content">
-        <h2>学習内容</h2>
-        <p>{{ content }}</p>
-      </div>
+        <h1>ダッシュボード</h1>
+        <ul>
+            <li v-for="category in categories" :key="category.id">
+                <button @click="fetchCategoryContent(category.id)">
+                    {{ category.name }}
+                </button>
+            </li>
+        </ul>
+        <div v-if="content">
+            <h2>学習内容</h2>
+            <p>{{ content }}</p>
+        </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
+</template>
+
+<script>
+export default {
     name: "DashboardPage",
     data() {
-      return {
-        categories: [],
-        content: null,
-      };
+        return {
+            categories: [],
+            content: null,
+        };
     },
     mounted() {
-      // ダッシュボードのカテゴリーをAPIから取得
-      fetch("http://localhost:8888/dashboard")
-        .then((response) => response.json())
-        .then((data) => {
-          this.categories = data.categories;
-        });
+        // ダッシュボードのカテゴリーをAPIから取得
+        fetch("http://localhost:8888/dashboard")
+            .then((response) => response.json())
+            .then((data) => {
+                this.categories = data.categories;
+            });
     },
     methods: {
-      fetchCategoryContent(categoryId) {
-        // カテゴリーを選択した際の学習内容をAPIから取得
-        fetch(`http://localhost:8888/category/${categoryId}`)
-          .then((response) => response.json())
-          .then((data) => {
-            this.content = data.content;
-          });
-      },
+        fetchCategoryContent(categoryId) {
+            // カテゴリーを選択した際の学習内容をAPIから取得
+            fetch(`http://localhost:8888/category/${categoryId}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    this.content = data.content;
+                });
+        },
     },
-  };
-  </script>
-  
+};
+</script>
